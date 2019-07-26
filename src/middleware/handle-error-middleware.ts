a56bc621041a0341cce26ed7bfcc25bdce654a78
Sidhant Panda <sidhantpanda@gmail.com>
2019-07-26 19:31:16 +0200

    Add documentation for middleware

--- src/middleware/handle-error-middleware.ts
+++ src/middleware/handle-error-middleware.ts
@@ -1,5 +1,12 @@
 import { RequestHandler } from 'express';
 
+/**
+ * This router wrapper catches any error from async await
+ * and throws it to the default express error handler,
+ * instead of crashing the app
+ * 
+ * @param handler Request handler to check for error
+ */
 const handleErrorMiddleware = (handler: RequestHandler): RequestHandler => async (req, res, next) => {
     handler(req, res, next).catch(next);
 };
