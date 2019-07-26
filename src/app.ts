b5520d1a9f7aa7c6556018144eed705a2a7fc761
Sidhant Panda <sidhantpanda@gmail.com>
2019-07-26 17:14:31 +0200

    add eslint and prettier

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,22 @@
-import bodyParser from "body-parser";
-import compression from "compression";
-import express from "express";
+import bodyParser from 'body-parser';
+import compression from 'compression';
+import express from 'express';
 const app = express();
 
-import HomeRouter from "./router/home";
-import UserRouter from "./router/user";
+import HomeRouter from './router/home';
+import UserRouter from './router/user';
 
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.set("port", process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000);
 
-app.use("/", HomeRouter);
-app.use("/user", UserRouter);
+app.use('/', HomeRouter);
+app.use('/user', UserRouter);
 // define a route handler for the default home page
-app.get("/", (req, res) => {
-  res.send("Hello world!");
+app.get('/', (req, res) => {
+    res.send('Hello world!');
 });
 
 export default app;
