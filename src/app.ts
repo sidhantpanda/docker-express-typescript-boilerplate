64aa09ccce5d56f176f28e81cf63ec88f8ec8324
Sidhant Panda <sidhantpanda@gmail.com>
2019-09-06 00:39:25 +0530

    add book router to app

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import bodyParser from 'body-parser';
 import compression from 'compression';
 import express from 'express';
-const app = express();
 
 import HomeRouter from './router/home';
+import BookRouter from './router/book';
 import UserRouter from './router/user';
+
+const app = express();
 
 app.use(compression());
 app.use(bodyParser.json());
@@ -13,6 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('port', process.env.PORT || 3000);
 
 app.use('/', HomeRouter);
+app.use('/book', BookRouter);
 app.use('/user', UserRouter);
 
 export default app;
