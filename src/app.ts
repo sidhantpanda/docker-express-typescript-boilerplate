f587cd7df30b5ee9b699f0a17e4e71b10148fd2d
Sidhant Panda <sidhantpanda@gmail.com>
2019-09-06 15:31:36 +0530

    add swagger ui

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import compression from 'compression';
 import express from 'express';
 
 import BookRouter from './router/book';
+import DevRouter from './router/dev';
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('port', process.env.PORT || 3000);
 
+app.use('/dev', DevRouter);
 app.use('/book', BookRouter);
 
 export default app;
