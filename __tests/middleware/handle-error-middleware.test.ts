5ba0f35aab937d0a0c6cc4d8411973806a8c7039
Sidhant Panda <sidhantpanda@gmail.com>
2019-09-06 17:35:42 +0530

    add test cases for error handler middlerware

---
+++ __tests/middleware/handle-error-middleware.test.ts
@@ -0,0 +1,52 @@
+import { Request } from 'jest-express/lib/request';
+import handleErrorMiddleware from '../../src/middleware/handle-error-middleware';
+
+let request: any;
+let next: any;
+
+describe('Error Handling Middleware', () => {
+  beforeEach(() => {
+    next = jest.fn();
+    request = new Request('/users?sort=desc', {
+      headers: {
+        Accept: 'text/html',
+      },
+    });
+  });
+
+  afterEach(() => {
+    request.resetMocked();
+    next.mockClear();
+  });
+
+  test('API handles response behaviour when no error thrown', async () => {
+    const res: any = {
+      send: jest.fn()
+    };
+
+    expect(next).toHaveBeenCalledTimes(0);
+    const sampleRoute = async (req?: any, res?: any, next?: any) => {
+      res.send();
+    };
+
+    const wrappedRoute = handleErrorMiddleware(sampleRoute);
+    await wrappedRoute(request, res, next);
+
+    expect(next).toHaveBeenCalledTimes(0);
+    expect(res.send).toHaveBeenCalledTimes(1);
+  });
+
+  test('Response 500 if route throws error', async () => {
+    const next = jest.fn();
+    const err = new Error('test error');
+    const sampleRoute = async (req?: any, res?: any, next?: any) => {
+      throw err;
+    };
+
+    const wrappedRoute = handleErrorMiddleware(sampleRoute);
+    await wrappedRoute(request, null, next);
+
+    expect(next).toHaveBeenCalledTimes(1);
+    expect(next).toHaveBeenCalledWith(err);
+  });
+});
