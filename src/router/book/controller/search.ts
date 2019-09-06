f1509a625d162c07c83d07b00ce14411fec53804
Sidhant Panda <sidhantpanda@gmail.com>
2019-09-06 15:00:42 +0530

    add ability for partial text seach

--- src/router/book/controller/search.ts
+++ src/router/book/controller/search.ts
@@ -2,13 +2,28 @@ import { RequestHandler } from 'express';
 import handleErrorMiddleware from '../../../middleware/handle-error-middleware';
 import Book from '../../../models/Book';
 
-let get: RequestHandler = async (req, res) => {
-  const { author, name } = req.query;
-  const books = await Book.find({
-    name: new RegExp(`^${name}$`, 'i'),
-    author: new RegExp(`^${author}$`, 'i'),
-  });
+/**
+ * Builds a mongoose query object to search books according to book name and author name.
+ * @param name String containing the book name or part of the book's name
+ * @param author String containing the author name or part of the author's name
+ */
+const buildBookSeachQuery = (name: string, author: string) => {
+  const query: any = {};
+  if (name) {
+    query.name = new RegExp(`.*${name}.*`, 'i');
+  }
+  if (author) {
+    query.author = new RegExp(`.*${author}.*`, 'i');
+  }
 
+  return query;
+}
+
+let get: RequestHandler = async (req, res) => {
+  const { name, author } = req.query;
+
+  const query = buildBookSeachQuery(name, author);
+  const books = await Book.find(query);
   res.send({ books });
 };
 
