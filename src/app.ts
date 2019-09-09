045b1c112dd655b039f26f5e8f64983a5ac6a8a7
Sidhant Panda <sidhantpanda@gmail.com>
2019-09-09 14:17:57 +0530

    add test cases

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 import bodyParser from 'body-parser';
 import compression from 'compression';
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 
 import BookRouter from './router/book';
 import DevRouter from './router/dev';
+import { ApplicationError } from './errors';
 
 const app = express();
 
@@ -18,5 +19,16 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 app.use('/book', BookRouter);
+
+app.use((err: ApplicationError, req: Request, res: Response, next: NextFunction) => {
+  if (res.headersSent) {
+    return next(err);
+  }
+  
+  return res.status(err.status || 500).json({
+    error: process.env.NODE_ENV === 'development' ? err : undefined,
+    message: err.message
+  });
+});
 
 export default app;
---
+++ src/errors/index.ts
@@ -0,0 +1,13 @@
+export class ApplicationError extends Error {
+  public message: string = 'ApplicationError';
+  public status: number = 500;
+  constructor(message?: string, status?: number) {
+    super();
+    if (message != null) {
+      this.message = message;
+    }
+    if (status != null) {
+      this.status = status;
+    }
+  }
+}
