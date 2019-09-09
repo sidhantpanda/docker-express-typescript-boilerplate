0c90aa98b96138bbde5258780ca4e6abfb06b186
Sidhant Panda <sidhantpanda@gmail.com>
2019-09-10 01:50:25 +0530

    add inline documentation

--- src/mongo-connection.ts
+++ src/mongo-connection.ts
@@ -1,13 +1,16 @@
 import mongoose from 'mongoose';
 
+/**
+ * Mongoose Connection Helper
+ * Connects to mongodb reliably with retries
+ */
 export default class MongoConnection {
   private mongoUrl: string;
   private onConnectedCallback: Function;
   private isConnectedBefore: boolean = false;
 
   /**
-   * Start mongo connection
-   * @param onConnected Function to be called when mongo connection is successful
+   * @param mongoUrl MongoDB connection url example: mongodb://localhost:27017/books
    */
   constructor(mongoUrl: string) {
     this.mongoUrl = mongoUrl;
@@ -18,14 +21,18 @@ export default class MongoConnection {
   }
 
   /**
-   * 
-   * @param onClosed 
+   * Close connection to MongoDB
+   * @param onClosed `err` passed as first argument in callback if there was an error while disconnecting
    */
   public close(onClosed: (err: Error) => void) {
     console.log('Closing the MongoDB conection');
     mongoose.connection.close(onClosed);
   }
 
+  /**
+   * Attempt to connect to Mongo
+   * @param onConnectedCallback Function to be called when connection is extablished
+   */
   public connect(onConnectedCallback?: Function) {
     if (onConnectedCallback) {
       this.onConnectedCallback = onConnectedCallback;
@@ -33,19 +40,31 @@ export default class MongoConnection {
     mongoose.connect(this.mongoUrl, { useNewUrlParser: true }, () => { });
   }
 
+  /**
+   * `onConnected` callback for mongoose
+   */
   private onConnected = () => {
     this.isConnectedBefore = true;
     this.onConnectedCallback();
   }
 
+  /**
+   * `onReconnected` callback for mongoose
+   */
   private onReconnected = () => {
     console.log('Reconnected to MongoDB');
   }
 
+  /**
+   * `onError` callback for mongoose
+   */
   private onError = () => {
     console.error(`Could not connect to MongoDB at ${this.mongoUrl}`);
   }
 
+  /**
+  * `onDisconnected` callback for mongoose
+  */
   private onDisconnected = () => {
     if (!this.isConnectedBefore) {
       console.log('Retrying MongoDB connection');
