4e074d840a122af05b4cba540f226dd6e15b4efe
Sidhant Panda <sidhantpanda@gmail.com>
2019-09-16 15:24:34 +0530

    add app test suite

---
+++ __tests/app.test.ts
@@ -0,0 +1,14 @@
+import request from 'supertest';
+import app from '../src/app';
+jest.mock('../src/models/Book');
+
+describe('App Test', () => {
+  test('GET /random-url should return 404', (done) => {
+    request(app).get('/reset')
+      .expect(404, done);
+  });
+
+  test('GET /book/all should return 200', (done) => {
+    request(app).get('/book/all').expect(200, done);
+  });
+});
