3e60ab5a4c3034776adb4bbffbe8438d87a46c2e
Sidhant Panda <sidhantpanda@gmail.com>
2020-05-01 16:51:11 +0200

    :bug: fix env variable resolution

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
+/* eslint-disable import/first */
 import dotenv from 'dotenv';
-import app from './app';
-import MongoConnection from './mongo-connection';
-import logger from './logger';
 
 const result = dotenv.config();
 if (result.error) {
   dotenv.config({ path: '.env.default' });
 }
+
+import app from './app';
+import MongoConnection from './mongo-connection';
+import logger from './logger';
 
 const mongoConnection = new MongoConnection(process.env.MONGO_URL);
 
