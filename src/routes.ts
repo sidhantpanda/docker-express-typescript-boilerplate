17aa937a61a3eabf3516e3b793600143ad7ad73c
Sidhant Panda <sidhantpanda@gmail.com>
2022-01-17 03:17:24 +0530

    🚚 Update conflicing routes

--- src/routes.ts
+++ src/routes.ts
@@ -13,9 +13,9 @@ const router = Router();
 // Book routes
 router.post('/book/add', BookController.add);
 router.get('/book/all', BookController.all);
-router.get('/book/:bookId', BookController.get);
-router.delete('/book/:bookId', BookController.remove);
 router.get('/book/search', BookController.search);
+router.get('/book/id/:bookId', BookController.get);
+router.delete('/book/id/:bookId', BookController.remove);
 
 // Dev routes
 if (process.env.NODE_ENV === 'development') {
