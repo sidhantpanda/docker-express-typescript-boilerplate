4d9c5304a4df2411428bc01a1ffcd965836089d9
Sidhant Panda <sidhantpanda@gmail.com>
2022-02-02 20:40:16 +0000

    🐛 Fix navigation to swagger

--- src/routes.ts
+++ src/routes.ts
@@ -20,14 +20,7 @@ router.delete('/book/id/:bookId', BookController.remove);
 // Dev routes
 if (process.env.NODE_ENV === 'development') {
   router.use('/dev/api-docs', swaggerUi.serve);
-  router.get('/dev/api-docs', (req, res, next) => {
-    const swaggerHost = `${req.protocol}://${req.headers.host}/`;
-    const newSpec = { ...apiSpec };
-    if (req.headers.host && req.protocol) {
-      newSpec.servers.push({ url: swaggerHost });
-    }
-    swaggerUi.setup(newSpec, swaggerUiOptions)(req, res, next);
-  });
+  router.get('/dev/api-docs', swaggerUi.setup(apiSpec, swaggerUiOptions));
 }
 
 export default router;
