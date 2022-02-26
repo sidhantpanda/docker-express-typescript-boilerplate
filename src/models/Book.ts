ec54a8ef77b93779c8f148ac766c2fc05c363141
Sidhant Panda <sidhantpanda@gmail.com>
2022-02-26 16:55:49 +0100

    Remove timestamp plugin

--- src/models/Book.ts
+++ src/models/Book.ts
@@ -1,15 +1,14 @@
 import {
-  Model, Schema, model
+  Model, Schema, model, Document
 } from 'mongoose';
-import TimeStampPlugin, {
-  ITimeStampedDocument
-} from './plugins/timestamp-plugin';
 
-export interface IBook extends ITimeStampedDocument {
+export interface IBook extends Document {
   /** Name of the book */
   name: string;
   /** Name of the author */
   author: string;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 interface IBookModel extends Model<IBook> { }
@@ -17,10 +16,7 @@ interface IBookModel extends Model<IBook> { }
 const schema = new Schema<IBook>({
   name: { type: String, index: true, required: true },
   author: { type: String, index: true, required: true }
-});
-
-// Add timestamp plugin for createdAt and updatedAt in miliseconds from epoch
-schema.plugin(TimeStampPlugin);
+}, { timestamps: true });
 
 const Book: IBookModel = model<IBook, IBookModel>('Book', schema);
 
