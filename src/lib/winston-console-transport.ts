62f808f6200cffb3fa6b31067c8b728b52e503d4
Sidhant Panda <sidhantpanda@gmail.com>
2023-04-11 01:47:20 +0200

    🔥 Remove empty lines

--- src/lib/winston-console-transport.ts
+++ src/lib/winston-console-transport.ts
@@ -14,11 +14,9 @@ const levelStyleMap: { [key: string]: string } = {
 };
 
 export default class ConsoleLogTransport extends Transport {
-
   log(info: any, callback: { (): void }) {
     const label = info.consoleLoggerOptions?.label! || (info.level as string).toUpperCase();
     const finalMessage = `[${new Date().toISOString()}] [${label}] ${info.message}`;
-
     console.log(levelStyleMap[info.level], finalMessage);
     info.stack && console.log('\t', info.stack);
     callback();
